b22c40c63d77ed0c6e2a3641def311af57f614ac
Developer Yousuf <hmyousuf2010@gmail.com>
2024-10-19 00:11:27 +0600

    dfsf

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['*'],
+  darkMode: "class",
+  theme: {
+    extend: {
+      colors: {
+        darkClr: "#22252D"
+      },
+      fontFamily: {
+        roboto : "Roboto",
+        cursive: "cursive"
+      }
+    },
+  },
+  plugins: [],
+}
+
